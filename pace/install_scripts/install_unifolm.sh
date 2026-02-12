76937d1bba4992fc9d9dcee9e917a81731e4c31e
hppeng <hppeng>
2026-02-11 22:02:30 -0500

    Add patch for run script

--- pace/install_scripts/install_unifolm.sh
+++ pace/install_scripts/install_unifolm.sh
@@ -7,6 +7,7 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "$BASH_SOURCE[0]")/" && pwd)
 cd "$SCRIPT_DIR/../../unifolm-world-model-action"
 git submodule init
 git submodule update --recursive
+git apply '../pace/install_scripts/unifolm/patch'
 
 uv pip install -e . --no-deps
 uv pip install gradio decord xformers libclang scikit-learn pytorch-lightning kornia torch==2.7.1 open_clip_torch==2.12.0
