7ed64d1bfb14538634efa88c8dae68334baa8c04
hppeng <hppeng>
2026-03-07 00:49:28 +0000

    Delete unused py scripts

--- py_script/ffmpeg_test.py
+++
@@ -1,9 +0,0 @@
-import mediapy
-
-import numpy as np
-
-frames = []
-for i in range(30):
-    frames.append(np.array(np.random.random((256, 256)) * 255, dtype=np.uint8))
-
-mediapy.write_video('test.mp4', frames, fps=30)
