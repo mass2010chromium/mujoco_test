b85388f2203d0f610c901092f6766c4d1ebfbe0e
jpeng303 <jpeng303@login1.vista.tacc.utexas.edu>
2026-03-19 21:02:04 -0500

    ffmpeg and SAM install on TACC
    decord with patch
    select ffmpeg version (is this needed?)

--- pace/install_scripts/build_ffmpeg.sh
+++ pace/install_scripts/build_ffmpeg.sh
@@ -11,11 +11,11 @@ mkdir -p installs
 cd installs
 git clone https://code.videolan.org/videolan/x264.git
 cd x264
-srun -c 12 bash "$SCRIPT_DIR/srun_x264.sh" "$PREFIX"
+srun -c 12 -N1 -t120 bash "$SCRIPT_DIR/srun_x264.sh" "$PREFIX"
 make install
 
 cd ..
 git clone https://git.ffmpeg.org/ffmpeg.git
 cd ffmpeg
-srun -c 12 bash "$SCRIPT_DIR/srun_ffmpeg.sh" "$PREFIX"
+srun -c 12 -N1 -t120 bash "$SCRIPT_DIR/srun_ffmpeg.sh" "$PREFIX"
 make install
--- pace/install_scripts/srun_ffmpeg.sh
+++ pace/install_scripts/srun_ffmpeg.sh
@@ -3,5 +3,6 @@
 PREFIX=$1
 PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"
 module load nasm/2.16.03
+git checkout n4.4.6 # Old version of ffmpeg, for decord
 ./configure --enable-shared --enable-libx264 --enable-gpl --enable-rpath --prefix="$PREFIX"
 make -j12
