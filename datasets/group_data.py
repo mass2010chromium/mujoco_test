00c68591aeae3f0100a7f4a6ddc50d391a41e86a
jpeng303 <jpeng303@login1.vista.tacc.utexas.edu>
2026-04-02 00:33:46 -0500

    Recording from multiple tokens instead of final token

--- datasets/group_data.py
+++ datasets/group_data.py
@@ -36,7 +36,7 @@ def get_sample(task_name, instance):
     target = np.load(data_dir / f"{task_name}_{instance}_transform.npy")
     intermediate = jnp.load(data_dir / f"{task_name}_{instance}_intermediate.npy")
     # time, layer, batch, token, dimension
-    return intermediate[:, :, 0, -1, :], target
+    return intermediate[:, [9], 0, -20:, :], target
 
     # time, layer, batch, token, dimension
     # But for kv cache intermediates (layer is 1, all tokens (~1000) but dimension 256 instead of 2048)
